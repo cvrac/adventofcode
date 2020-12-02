5dbbae48171bc1ab77456dd70cd50a69ff6dcee0
Christos Vrachas <cvracdi@gmail.com>
2020-12-02 19:22:38 +0200

    Update

--- day2/day2.rb
+++ day2/day2.rb
@@ -1,16 +1,15 @@
 lines = File.read('input.txt').split("\n")
 
+puts '----part1----'
 passwords_matching = lines.count do |line|
   /(?<min>\d*)-(?<max>\d*) (?<letter>\w): (?<password>.*)/ =~ line
 
   password.count(letter).between?(min.to_i, max.to_i)
 end
 
-puts '----part1----'
 puts passwords_matching
 
 puts '----part2----'
-
 passwords_matching_2 = lines.count do |line|
   /(?<min>\d*)-(?<max>\d*) (?<letter>\w): (?<password>.*)/ =~ line
 
